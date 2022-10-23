4bfb69ce8e510a6c68b5b404c9274f24a2223938
Simon Dániel <h051356@stud.u-szeged.hu>
2022-10-24 00:04:21 +0200

    Firebase beállítás

--- Angular project/SocialMedia/src/environments/environment.ts
+++ Angular project/SocialMedia/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'rf1-2022-kobweb',
+    appId: '1:1070112355930:web:05643763d8064702714a63',
+    storageBucket: 'rf1-2022-kobweb.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCjQmlO2azk0QKurAkqgsF8VFOycboOF7M',
+    authDomain: 'rf1-2022-kobweb.firebaseapp.com',
+    messagingSenderId: '1070112355930',
+  },
   production: false
 };
 
