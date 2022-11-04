285839a45fd455e6192c78ed44747cfd5ea615a4
losonczimate <merlonmerlon2@gmail.com>
2022-11-04 18:57:47 +0100

    Regisztrációs oldal néhány javításra várva, de szerkezetileg készen van.

--- Angular project/SocialMedia/src/app/Pages/registration/registration.component.ts
+++ Angular project/SocialMedia/src/app/Pages/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  regForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rePassword: new FormControl(''),
+    nickname: new FormControl(''),
+
+  });
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+
+  }
+
+  goBack() {
+    //this.location.back();
+  }
 }
--- Angular project/SocialMedia/src/app/Pages/registration/registration.module.ts
+++ Angular project/SocialMedia/src/app/Pages/registration/registration.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { RegistrationComponent } from './registration.component';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule
   ]
 })
 export class RegistrationModule { }
