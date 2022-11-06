c3cb530783f26924f0af59d010ba16fc577f870e
valti01 <valti0190@gmail.com>
2022-11-06 22:52:29 +0100

    Reklámokhoz CRUD bővítése ID alapján kereséssel

---
+++ Angular project/SocialMedia/src/app/Shared/services/advertisements.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {Advertisement} from "../../Model/advertisement";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdvertisementsService {
+
+  collectionName = 'Advertisements';
+
+  constructor(private afs: AngularFirestore) { }
+
+  create(ad: Advertisement) {
+    ad.id = this.afs.createId();
+    return this.afs.collection<Advertisement>(this.collectionName).doc(ad.id).set(ad);
+    // return this.afs.collection<Comment>(this.collectionName).add(comment);
+  }
+
+  getAll() {
+    return this.afs.collection<Advertisement>(this.collectionName).valueChanges();
+  }
+
+  update(ad: Advertisement) {
+    return this.afs.collection<Advertisement>(this.collectionName).doc(ad.id).set(ad);
+  }
+
+  delete(id: string) {
+    return this.afs.collection<Advertisement>(this.collectionName).doc(id).delete();
+  }
+
+  getById(id: string) {
+    return this.afs.collection<Advertisement>(this.collectionName).doc(id).valueChanges();
+  }
+}
