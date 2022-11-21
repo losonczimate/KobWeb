ee4d11edc6d248350304486af010692cbdabff66
Bondar Kristof <h043327@stud.u-szeged.hu>
2022-11-21 20:56:57 +0100

    Mobil autentikáció folytatása, otp generálás adott telefonszámhoz.

--- Angular project/SocialMedia/src/app/Pages/loginphonenumber/loginphonenumber.component.ts
+++ Angular project/SocialMedia/src/app/Pages/loginphonenumber/loginphonenumber.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthService} from "../../Shared/services/auth.service";
+import firebase from "firebase/compat/app";
+import 'firebase/auth';
+import 'firebase/firestore';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-loginphonenumber',
@@ -9,7 +13,8 @@ import {AuthService} from "../../Shared/services/auth.service";
   styleUrls: ['./loginphonenumber.component.scss']
 })
 export class LoginphonenumberComponent implements OnInit {
-
+  reCaptchaVerifier: any;
+  phoneNumber: any;
 
   phoneForm = new FormGroup({
     phonenumber: new FormControl('')
@@ -20,9 +25,20 @@ export class LoginphonenumberComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(){
+  getOTP(){
+    this.reCaptchaVerifier = new firebase.auth.RecaptchaVerifier('belepo', {size: 'invisible'})
     if(this.phoneForm.valid){
       if(this.phoneForm.get('phonenumber')?.value){
+        this.phoneNumber = this.phoneForm.get('phonenumber')?.value;
+        firebase.auth().signInWithPhoneNumber(this.phoneNumber, this.reCaptchaVerifier).then((conf) => {
+          localStorage.setItem('verificationId', JSON.stringify(conf.verificationId));
+          this.router.navigateByUrl('/otp');
+        }).catch(error => {
+          console.log(error);
+          setTimeout(() => {
+            window.location.reload()
+          }, 3000);
+        });
       }
     }
   }
